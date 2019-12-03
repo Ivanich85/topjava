34e77b382e69566af71abbd58fe8217187e9eadc
Ivanich85 <ivan.dmitriev85@gmail.com>
2019-12-03 23:40:33 +0300

    HW08 done WO point 5

--- src/main/webapp/resources/js/topjava.common.js
+++ src/main/webapp/resources/js/topjava.common.js
@@ -33,9 +33,54 @@ function deleteRow(id) {
 }
 
 function updateTable() {
+    if (isFilterReset()) {
+        updateTableWOFilter();
+    } else {
+        updateFilteredTable();
+    }
+}
+
+function updateTableWOFilter() {
     $.get(context.ajaxUrl, function (data) {
         context.datatableApi.clear().rows.add(data).draw();
     });
+}
+
+function updateFilteredTable() {
+    $.ajax({
+        url: context.ajaxUrl + "filter",
+        type: "GET",
+        data: {
+            startDate: $("input[name='startDate']").val(),
+            endDate: $("input[name='endDate']").val(),
+            startTime: $("input[name='startTime']").val(),
+            endTime: $("input[name='endTime']").val(),
+        }
+    }).done(function (data) {
+        context.datatableApi.clear().rows.add(data).draw();
+        successNoty("Filtered");
+    });
+}
+
+function isFilterReset() {
+    let result = true;
+    $("#filters").find($("input")).each(function(){
+        if ($(this).val()) {
+            result = false;
+        }
+    });
+    return result;
+}
+
+function resetFilters() {
+    $("#filters").find($("input")).each(function(){
+        let elem = $(this);
+        if (elem.val()) {
+            elem.val("");
+        }
+    });
+    updateTable();
+    successNoty("Filters were reset");
 }
 
 function save() {
@@ -48,34 +93,6 @@ function save() {
         updateTable();
         successNoty("Saved");
     });
-}
-
-let failedNote;
-
-function closeNoty() {
-    if (failedNote) {
-        failedNote.close();
-        failedNote = undefined;
-    }
-}
-
-function successNoty(text) {
-    closeNoty();
-    new Noty({
-        text: "<span class='fa fa-lg fa-check'></span> &nbsp;" + text,
-        type: 'success',
-        layout: "bottomRight",
-        timeout: 1000
-    }).show();
-}
-
-function failNoty(jqXHR) {
-    closeNoty();
-    failedNote = new Noty({
-        text: "<span class='fa fa-lg fa-exclamation-circle'></span> &nbsp;Error status: " + jqXHR.status,
-        type: "error",
-        layout: "bottomRight"
-    }).show();
 }
 
 let failedNote;
